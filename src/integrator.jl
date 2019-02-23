6575bb6369a825e16d477ca80d0493e99c196ffc
Kai Xu <xukai921110@gmail.com>
2019-02-23 03:06:53 +0000

    add numerical check for leapfrog

--- src/integrator.jl
+++ src/integrator.jl
@@ -5,19 +5,48 @@ struct Leapfrog{T<:Real} <: AbstractIntegrator
     ϵ   ::  T
 end
 
-function step(l::Leapfrog{T}, h::Hamiltonian, θ::AbstractVector{T}, r::AbstractVector{T}) where {T<:Real}
-    r = r - l.ϵ / 2 .* _dHdθ(h, θ)
-    θ = θ + l.ϵ     .* _dHdr(h, r)
-    r = r - l.ϵ / 2 .* _dHdθ(h, θ)
-    return θ, r
-end
-
-# TODO: improve below
-function step(l::Leapfrog{T}, h::Hamiltonian, θ::AbstractVector{T}, r::AbstractVector{T}, n_steps::Integer) where {T<:Real}
-    for _ = 1:n_steps
-        θ, r = step(l, h, θ, r)
+function _is_valid(v::AbstractVector{<:Real})
+    if any(isnan, v) || any(isinf, v)
+        return false
     end
-    return θ, r
+    return true
 end
 
-# TODO: implement a version which returns all samples in the trajectory
+function _lf_momentum(ϵ::T, h::Hamiltonian, θ::AbstractVector{T}, r::AbstractVector{T}) where {T<:Real}
+    dHdθ = _dHdθ(h, θ)
+    !_is_valid(dHdθ) && return r, false
+    return r - ϵ .* dHdθ, true
+end
+
+function _lf_position(ϵ::T, h::Hamiltonian, θ::AbstractVector{T}, r::AbstractVector{T}) where {T<:Real}
+    return θ + ϵ .* _dHdr(h, r)
+end
+
+function step(l::Leapfrog{T}, h::Hamiltonian, θ::AbstractVector{T}, r::AbstractVector{T}) where {T<:Real}
+    r_new, is_valid = _lf_momentum(l.ϵ / 2, h, θ, r)
+    !is_valid && return θ, r, false
+    θ_new = _lf_position(l.ϵ, h, θ, r_new)
+    r_new, is_valid = _lf_momentum(l.ϵ / 2, h, θ_new, r_new)
+    !is_valid && return θ, r, false
+    return θ_new, r_new, true
+end
+
+function steps(l::Leapfrog{T}, h::Hamiltonian, θ::AbstractVector{T}, r::AbstractVector{T}, n_steps::Integer) where {T<:Real}
+    n_valid = 0
+    r_new, is_valid = _lf_momentum(l.ϵ / 2, h, θ, r)
+    !is_valid && return θ, r, n_valid
+    r = r_new
+    for i = 1:n_steps
+        θ_new = _lf_position(l.ϵ, h, θ, r)
+        r_new, is_valid = _lf_momentum(i == n_steps ? l.ϵ / 2 : l.ϵ, h, θ, r)
+        if !is_valid
+            # The reverse function below is guarantee to be numerical safe.
+            # This is because we know the previous step was valid.
+            r, _ = _lf_momentum(-l.ϵ / 2, h, θ, r)
+            return θ, r, n_valid
+        end
+        θ, r = θ_new, r_new
+        n_valid = n_valid + 1
+    end
+    return θ, r, n_valid
+end
--- src/trajectory.jl
+++ src/trajectory.jl
@@ -5,13 +5,12 @@ struct StaticTrajectory <: AbstractTrajectory
     n_points    ::  Integer
 end
 
-# TODO: implement a version which returns all points in the trajectory
 function points(st::StaticTrajectory)
-
+    return [step(st.integrator, h, θ, r) for i = 1:st.n_points]
 end
 
 function lastpoint(st::StaticTrajectory, h::Hamiltonian, θ::AbstractVector{T}, r::AbstractVector{T}) where {T<:Real}
-    θ, r = step(st.integrator, h, θ, r, st.n_points)
+    return steps(st.integrator, h, θ, r, st.n_points)
 end
 
 struct NoUTurnTrajectory
