34cde8b782ddcd1e730aed7893645ee452c002b9
Kai Xu <xukai921110@gmail.com>
2019-02-25 17:41:25 +0000

    add @time in tests

--- test/integrator.jl
+++ test/integrator.jl
@@ -13,11 +13,11 @@ n_steps = 10
 @testset "step() against steps()" begin
     θ1, r1 = copy(θ_init), copy(r_init)
 
-    for i = 1:n_steps
+    @time for i = 1:n_steps
         θ1, r1, _ = HMC.step(lf, h, θ1, r1)
     end
 
-    θ2, r2, _ = HMC.steps(lf, h, θ_init, r_init, n_steps)
+    @time θ2, r2, _ = HMC.steps(lf, h, θ_init, r_init, n_steps)
 
     @test θ1 ≈ θ2 atol=DETATOL
     @test r1 ≈ r2 atol=DETATOL
@@ -25,8 +25,8 @@ end
 
 using Turing: Inference
 @testset "steps() against Turing.Inference._leapfrog()" begin
-    θ1, r1, _ = Inference._leapfrog(θ_init, r_init, n_steps, ϵ, x -> (nothing, -_dlogπdθ(x)))
-    θ2, r2, _ = HMC.steps(lf, h, θ_init, r_init, n_steps)
+    @time θ1, r1, _ = Inference._leapfrog(θ_init, r_init, n_steps, ϵ, x -> (nothing, -_dlogπdθ(x)))
+    @time θ2, r2, _ = HMC.steps(lf, h, θ_init, r_init, n_steps)
 
     @test θ1 ≈ θ2 atol=DETATOL
     @test r1 ≈ r2 atol=DETATOL
