84a20ee3a3dc6616531660fa15eac7475b8fa9d1
Kai Xu <xukai921110@gmail.com>
2019-03-08 20:26:42 +0000

    rename HMC to AdvancedHMC and fix tests

--- test/proposal.jl
+++ test/proposal.jl
@@ -1,4 +1,4 @@
-using Test, HMC
+using Test, AdvancedHMC
 using Random
 include("common.jl")
 
@@ -8,14 +8,14 @@ lf = Leapfrog(ϵ)
 θ_init = randn(D)
 h = Hamiltonian(UnitEuclideanMetric(θ_init), logπ, ∂logπ∂θ)
 prop = SliceNUTS(Leapfrog(find_good_eps(h, θ_init)))
-r_init = HMC.rand_momentum(h)
+r_init = AdvancedHMC.rand_momentum(h)
 
 @testset "Passing random number generator" begin
     rng = MersenneTwister(1234)
-    θ1, r1 = HMC.propose(rng, prop, h, θ_init, r_init)
+    θ1, r1 = AdvancedHMC.propose(rng, prop, h, θ_init, r_init)
 
     rng = MersenneTwister(1234)
-    θ2, r2 = HMC.propose(rng, prop, h, θ_init, r_init)
+    θ2, r2 = AdvancedHMC.propose(rng, prop, h, θ_init, r_init)
 
     @test θ1 == θ2
     @test r1 == r2
