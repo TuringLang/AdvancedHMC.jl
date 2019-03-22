39324118e4045900f1c106ee03a6459b2b4758b9
Kai Xu <xukai921110@gmail.com>
2019-03-22 01:04:06 +0000

    add analytical tests for leapfrog #14

--- test/integrator.jl
+++ test/integrator.jl
@@ -31,3 +31,42 @@ using Turing: Inference
     @test θ1 ≈ θ2 atol=DETATOL
     @test r1 ≈ r2 atol=DETATOL
 end
+
+using LinearAlgebra: dot
+using Statistics: mean
+@testset "Eq (2.11) from Neal (2011)" begin
+    D = 1
+    negU(q::AbstractVector{T}) where {T<:Real} = -dot(q, q) / 2
+    ∂negU∂q = q -> gradient(negU, q)
+
+    ϵ = 0.1
+    lf = Leapfrog(ϵ)
+
+    q_init = randn(D)
+    h = Hamiltonian(UnitEuclideanMetric(q_init), negU, ∂negU∂q)
+    p_init = AdvancedHMC.rand_momentum(h)
+
+    q, p = copy(q_init), copy(p_init)
+
+    n_steps = 10_000
+    qs = zeros(n_steps)
+    ps = zeros(n_steps)
+    Hs = zeros(n_steps)
+    for i = 1:n_steps
+        q, p, _ = AdvancedHMC.step(lf, h, q, p)
+        qs[i] = q[1]
+        ps[i] = p[1]
+        Hs[i] = AdvancedHMC.hamiltonian_energy(h, q, p)
+    end
+
+    # Throw first 1_000 steps
+    qs = qs[1_000:end]
+    ps = ps[1_000:end]
+    Hs = Hs[1_000:end]
+
+    # Check if all points located at a cirle centered at the origin
+    rs = sqrt.(qs.^2 + ps.^2)
+    @test all(x-> abs(x - mean(rs)) < 1e-3, rs)
+    # Check if the Hamiltonian energy is stable
+    @test all(x-> abs(x - mean(Hs)) < 1e-3, Hs)
+end
