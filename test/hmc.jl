fe67fa7f78c6eb48e73325ffffcb5d206a37daa4
Kai Xu <xukai921110@gmail.com>
2019-03-23 14:27:38 +0000

    make testset more informative

--- test/hmc.jl
+++ test/hmc.jl
@@ -10,14 +10,17 @@ n_adapts = 1_000
 
 @testset "HMC and NUTS" begin
     temp = randn(D,100)
-    for metric in [UnitEuclideanMetric(θ_init), DiagEuclideanMetric(θ_init, vec(var(temp; dims=2))), DenseEuclideanMetric(θ_init, cov(temp'))]
+    @testset "$metric" for metric in [UnitEuclideanMetric(θ_init),
+                                      DiagEuclideanMetric(θ_init, vec(var(temp; dims=2))),
+                                      DenseEuclideanMetric(θ_init, cov(temp'))]
         h = Hamiltonian(metric, logπ, ∂logπ∂θ)
-        for prop in [TakeLastProposal(Leapfrog(ϵ), n_steps), SliceNUTS(Leapfrog(find_good_eps(h, θ_init)))]
+        @testset "$prop" for prop in [TakeLastProposal(Leapfrog(ϵ), n_steps),
+                                      SliceNUTS(Leapfrog(find_good_eps(h, θ_init)))]
             samples = AdvancedHMC.sample(h, prop, θ_init, n_samples; verbose=false)
             @test mean(samples) ≈ zeros(D) atol=RNDATOL
-            pc = PreConditioner(metric)
-            ssa = DualAveraging(0.8, prop.integrator.ϵ)
-            for adapter in [ssa, pc, NaiveCompAdapter(pc, ssa), ThreePhaseAdapter(n_adapts, pc, ssa)]
+            @testset "$adapter" for adapter in [PreConditioner(metric), DualAveraging(0.8, prop.integrator.ϵ),
+                                                NaiveCompAdapter(PreConditioner(metric), DualAveraging(0.8, prop.integrator.ϵ)),
+                                                ThreePhaseAdapter(n_adapts, PreConditioner(metric), DualAveraging(0.8, prop.integrator.ϵ))]
                 samples = AdvancedHMC.sample(h, prop, θ_init, n_samples, adapter, n_adapts; verbose=false)
                 @test mean(samples) ≈ zeros(D) atol=RNDATOL
             end
