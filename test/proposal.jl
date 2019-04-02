850a128db161516f303a53d0490f7d2f97f01a46
Kai Xu <xukai921110@gmail.com>
2019-04-02 17:45:24 +0100

    add more tests for RNG check

--- test/proposal.jl
+++ test/proposal.jl
@@ -11,12 +11,14 @@ prop = NUTS(Leapfrog(find_good_eps(h, θ_init)))
 r_init = AdvancedHMC.rand_momentum(h)
 
 @testset "Passing random number generator" begin
-    rng = MersenneTwister(1234)
-    θ1, r1 = AdvancedHMC.propose(rng, prop, h, θ_init, r_init)
+    for seed in [1234, 5678, 90]
+        rng = MersenneTwister(seed)
+        θ1, r1 = AdvancedHMC.propose(rng, prop, h, θ_init, r_init)
 
-    rng = MersenneTwister(1234)
-    θ2, r2 = AdvancedHMC.propose(rng, prop, h, θ_init, r_init)
+        rng = MersenneTwister(seed)
+        θ2, r2 = AdvancedHMC.propose(rng, prop, h, θ_init, r_init)
 
-    @test θ1 == θ2
-    @test r1 == r2
+        @test θ1 == θ2
+        @test r1 == r2
+    end
 end
