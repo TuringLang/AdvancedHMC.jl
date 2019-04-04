8fb1151062c072202de82ca6bb9b50f8684e69c3
Hong Ge <hg344@cam.ac.uk>
2019-04-04 20:06:43 +0100

    Fix some code styles - no functionality change.

--- src/integrator.jl
+++ src/integrator.jl
@@ -16,17 +16,25 @@ function is_valid(v::AbstractVector{<:Real})
     return true
 end
 
-function lf_momentum(ϵ::T, h::Hamiltonian, θ::AbstractVector{T}, r::AbstractVector{T}) where {T<:Real}
+function lf_momentum(ϵ::T,
+        h::Hamiltonian, θ::AbstractVector{T},
+        r::AbstractVector{T}
+    ) where {T<:Real}
     _∂H∂θ = ∂H∂θ(h, θ)
     !is_valid(_∂H∂θ) && return r, false
     return r - ϵ * _∂H∂θ, true
 end
 
-function lf_position(ϵ::T, h::Hamiltonian, θ::AbstractVector{T}, r::AbstractVector{T}) where {T<:Real}
+function lf_position(ϵ::T, h::Hamiltonian,
+        θ::AbstractVector{T}, r::AbstractVector{T}
+    ) where {T<:Real}
     return θ + ϵ * ∂H∂r(h, r)
 end
 
-function step(lf::Leapfrog{F}, h::Hamiltonian, θ::AbstractVector{T}, r::AbstractVector{T}) where {F<:AbstractFloat,T<:Real}
+function step(lf::Leapfrog{F},
+        h::Hamiltonian, θ::AbstractVector{T},
+        r::AbstractVector{T}
+    ) where {F<:AbstractFloat,T<:Real}
     r_new, _is_valid = lf_momentum(lf.ϵ / 2, h, θ, r)
     !_is_valid && return θ, r, false
     θ_new = lf_position(lf.ϵ, h, θ, r_new)
@@ -36,7 +44,10 @@ function step(lf::Leapfrog{F}, h::Hamiltonian, θ::AbstractVector{T}, r::Abstrac
 end
 
 # TODO: double check the function below to see if it is type stable or not
-function steps(lf::Leapfrog{F}, h::Hamiltonian, θ::AbstractVector{T}, r::AbstractVector{T}, n_steps::Int) where {F<:AbstractFloat,T<:Real}
+function steps(lf::Leapfrog{F},
+        h::Hamiltonian, θ::AbstractVector{T},
+        r::AbstractVector{T}, n_steps::Int
+    ) where {F<:AbstractFloat,T<:Real}
     n_valid = 0
     r_new, _is_valid = lf_momentum(lf.ϵ / 2, h, θ, r)
     !_is_valid && return θ, r, n_valid
