0d4aed334c70627ce4f5ec8dc5da9c26691c341a
Kai Xu <xukai921110@gmail.com>
2019-05-20 21:07:50 +0100

    test @warn in the constructor of PhasePoint

--- test/hamiltonian.jl
+++ test/hamiltonian.jl
@@ -9,8 +9,15 @@ r_init = ones(D)
 
 @test -AdvancedHMC.neg_energy(h, r_init, θ_init) == D / 2
 
-z1 = PhasePoint([NaN], [NaN], DualValue(0.,[0.]), DualValue(0.,[0.]))
-z2 = PhasePoint([Inf], [Inf], DualValue(0.,[0.]), DualValue(0.,[0.]))
+
+init_z1() = PhasePoint([NaN], [NaN], DualValue(0.,[0.]), DualValue(0.,[0.]))
+init_z2() = PhasePoint([Inf], [Inf], DualValue(0.,[0.]), DualValue(0.,[0.]))
+
+@test_logs (:warn, "The current proposal will be rejected (due to numerical error(s))...") init_z1()
+@test_logs (:warn, "The current proposal will be rejected (due to numerical error(s))...") init_z2()
+
+z1 = init_z1()
+z2 = init_z2()
 
 @test z1.ℓπ.value == z1.ℓπ.value
 @test z1.ℓκ.value == z1.ℓκ.value
