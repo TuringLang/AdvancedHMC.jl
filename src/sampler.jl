3c9140ee82fe112b675fb191ec87f58313a35bc9
Kai Xu <xukai921110@gmail.com>
2019-07-06 23:28:03 +0100

    draft a docstring for the sample function (#75)

--- src/sampler.jl
+++ src/sampler.jl
@@ -13,6 +13,27 @@ sample(
     progress::Bool=false
 ) where {T<:Real} = sample(GLOBAL_RNG, h, τ, θ, n_samples, adaptor, n_adapts; verbose=verbose, progress=progress)
 
+"""
+    sample(
+        rng::AbstractRNG,
+        h::Hamiltonian,
+        τ::AbstractProposal,
+        θ::AbstractVector{T},
+        n_samples::Int,
+        adaptor::Adaptation.AbstractAdaptor=Adaptation.NoAdaptation(),
+        n_adapts::Int=min(div(n_samples, 10), 1_000);
+        verbose::Bool=true,
+        progress::Bool=false
+    )
+
+Sample `n_samples` samples using the proposal `τ` under Hamiltonian `h`.
+- the initial point is given by `θ`
+- the randomness is controlled by `rng`
+- the adaptor is set by `adaptor`, for which the default is no adapation
+    - it will perform `n_adapts` steps of adapations, for which the default is the minimum of `1_000` and 10% of `n_samples`
+- the verbosity is controlled by the boolean variable `verbose` and
+- the visibility of the progress meter is controlled by the bollean variable `progress`
+"""
 function sample(
     rng::AbstractRNG,
     h::Hamiltonian,
