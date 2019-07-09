c828a04a0d021ea42565c89a9c0b7a504f6c88c8
Kai Xu <xukai921110@gmail.com>
2019-07-09 03:35:03 +0100

    improve test script

--- test/hmc.jl
+++ test/hmc.jl
@@ -5,41 +5,41 @@ using Test, AdvancedHMC, LinearAlgebra
 using Statistics: mean, var, cov
 include("common.jl")
 
-θ_init = randn(D)
+θ_init = 2rand(D)
 ϵ = 0.02
 n_steps = 20
 n_samples = 100_000
 n_adapts = 2_000
 
 @testset "HMC and NUTS" begin
-    @testset "$(typeof(metric))" for metric in [
-        UnitEuclideanMetric(D),
-        DiagEuclideanMetric(D),
-        DenseEuclideanMetric(D),
-    ]
+    @testset "$metricsym" for (metricsym, metric) in Dict(
+        :UnitEuclideanMetric => UnitEuclideanMetric(D),
+        :DiagEuclideanMetric => DiagEuclideanMetric(D),
+        :DenseEuclideanMetric => DenseEuclideanMetric(D),
+    )
         h = Hamiltonian(metric, logπ, ∂logπ∂θ)
-        @testset "$(typeof(τ))" for τ in [
-            StaticTrajectory(Leapfrog(ϵ), n_steps),
-            HMCDA(Leapfrog(ϵ), ϵ * n_steps),
-            NUTS(Leapfrog(find_good_eps(h, θ_init)); sampling=:multinomial),
-            NUTS(Leapfrog(find_good_eps(h, θ_init)); sampling=:slice),
-        ]
+        @testset "$τsym" for (τsym, τ) in Dict(
+            :HMC => StaticTrajectory(Leapfrog(ϵ), n_steps),
+            :HMCDA => HMCDA(Leapfrog(ϵ), ϵ * n_steps),
+            :MultinomialNUTS => NUTS(Leapfrog(find_good_eps(h, θ_init)); sampling=:multinomial),
+            :SliceNUTS => NUTS(Leapfrog(find_good_eps(h, θ_init)); sampling=:slice),
+        )
             samples = sample(h, τ, θ_init, n_samples; verbose=false, progress=PROGRESS)
             @test mean(samples[n_adapts+1:end]) ≈ zeros(D) atol=RNDATOL
 
-            @testset "$(typeof(adaptor))" for adaptor in [
-                Preconditioner(metric),
-                NesterovDualAveraging(0.8, τ.integrator.ϵ),
-                NaiveHMCAdaptor(
+            @testset "$adaptorsym" for (adaptorsym, adaptor) in Dict(
+                :PreconditionerOnly => Preconditioner(metric),
+                :NesterovDualAveragingOnly => NesterovDualAveraging(0.8, τ.integrator.ϵ),
+                :NaiveHMCAdaptor => NaiveHMCAdaptor(
                     Preconditioner(metric),
                     NesterovDualAveraging(0.8, τ.integrator.ϵ),
                 ),
-                StanHMCAdaptor(
+                :StanHMCAdaptor => StanHMCAdaptor(
                     n_adapts,
                     Preconditioner(metric),
                     NesterovDualAveraging(0.8, τ.integrator.ϵ),
                 ),
-            ]
+            )
                 samples = sample(h, τ, θ_init, n_samples, adaptor, n_adapts; verbose=false, progress=PROGRESS)
                 @test mean(samples[n_adapts+1:end]) ≈ zeros(D) atol=RNDATOL
             end
