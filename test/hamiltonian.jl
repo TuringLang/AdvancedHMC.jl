0743e1ae5047cebe2536f2dfbd3d3a87e0cd3cc3
Kai Xu <xukai921110@gmail.com>
2019-08-17 00:04:15 +0100

    add some unit tests for different metrics

--- test/hamiltonian.jl
+++ test/hamiltonian.jl
@@ -1,23 +1,44 @@
 using Test, AdvancedHMC
-import AdvancedHMC: DualValue, PhasePoint
+using AdvancedHMC: DualValue, PhasePoint
+using LinearAlgebra: dot, diagm
+
 
 include("common.jl")
 
-θ_init = randn(D)
-h = Hamiltonian(UnitEuclideanMetric(D), ℓπ, ∂ℓπ∂θ)
-r_init = ones(D)
+@testset "PhasePoint" begin
+    init_z1() = PhasePoint([NaN], [NaN], DualValue(0.,[0.]), DualValue(0.,[0.]))
+    init_z2() = PhasePoint([Inf], [Inf], DualValue(0.,[0.]), DualValue(0.,[0.]))
 
-@test -AdvancedHMC.neg_energy(h, r_init, θ_init) == D / 2
+    @test_logs (:warn, "The current proposal will be rejected due to numerical error(s).") init_z1()
+    @test_logs (:warn, "The current proposal will be rejected due to numerical error(s).") init_z2()
 
+    z1 = init_z1()
+    z2 = init_z2()
 
-init_z1() = PhasePoint([NaN], [NaN], DualValue(0.,[0.]), DualValue(0.,[0.]))
-init_z2() = PhasePoint([Inf], [Inf], DualValue(0.,[0.]), DualValue(0.,[0.]))
+    @test z1.ℓπ.value == z1.ℓπ.value
+    @test z1.ℓκ.value == z1.ℓκ.value
+end
 
-@test_logs (:warn, "The current proposal will be rejected due to numerical error(s).") init_z1()
-@test_logs (:warn, "The current proposal will be rejected due to numerical error(s).") init_z2()
+@testset "Metric" begin
+    n_tests = 10
 
-z1 = init_z1()
-z2 = init_z2()
+    for _ in 1:n_tests
+        θ_init = randn(D)
+        r_init = randn(D)
 
-@test z1.ℓπ.value == z1.ℓπ.value
-@test z1.ℓκ.value == z1.ℓκ.value
+        h = Hamiltonian(UnitEuclideanMetric(D), ℓπ, ∂ℓπ∂θ)
+        @test -AdvancedHMC.neg_energy(h, r_init, θ_init) == dot(r_init, r_init) / 2
+        @test AdvancedHMC.∂H∂r(h, r_init) == r_init
+
+        M⁻¹ = ones(D) + abs.(randn(D))
+        h = Hamiltonian(DiagEuclideanMetric(M⁻¹), ℓπ, ∂ℓπ∂θ)
+        @test -AdvancedHMC.neg_energy(h, r_init, θ_init) ≈ r_init' * diagm(0 => M⁻¹) * r_init / 2
+        @test AdvancedHMC.∂H∂r(h, r_init) == M⁻¹ .* r_init
+
+        m = randn(D, D)
+        M⁻¹ = m' * m
+        h = Hamiltonian(DenseEuclideanMetric(M⁻¹), ℓπ, ∂ℓπ∂θ)
+        @test -AdvancedHMC.neg_energy(h, r_init, θ_init) ≈ r_init' * M⁻¹ * r_init / 2
+        @test AdvancedHMC.∂H∂r(h, r_init) == M⁻¹ * r_init
+    end
+end
