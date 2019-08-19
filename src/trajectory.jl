30c40a592f6c915b3e3aa167e44454ef0301563b
Kai Xu <xukai921110@gmail.com>
2019-08-19 16:30:32 +0100

    improve naming in isterminated for ClassicNoUTurn

--- src/trajectory.jl
+++ src/trajectory.jl
@@ -331,8 +331,8 @@ Ref: https://arxiv.org/abs/1111.4246, https://arxiv.org/abs/1701.02434
 function isterminated(h::Hamiltonian, t::FullBinaryTree{ClassicNoUTurn})
     # z0 is starting point and z1 is ending point
     z0, z1 = t.zleft, t.zright
-    θ0minusθ1 = z0.θ - z1.θ
-    s = (dot(-θ0minusθ1, ∂H∂r(h, -z0.r)) >= 0) || (dot(θ0minusθ1, ∂H∂r(h, z1.r)) >= 0)
+    Δθ = z1.θ - z0.θ
+    s = (dot(Δθ, ∂H∂r(h, -z0.r)) >= 0) || (dot(-Δθ, ∂H∂r(h, z1.r)) >= 0)
     return Termination(s, false)
 end
 
