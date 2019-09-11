e6cbe5f6c717d24c825beb402f3381a4fbaefe67
Kai Xu <xukai921110@gmail.com>
2019-09-11 20:46:32 +0100

    avoid tempering if alpha is 1

--- src/integrator.jl
+++ src/integrator.jl
@@ -26,19 +26,19 @@ function step(
     sqrtα = sqrt(lf.α)
     n_steps = abs(n_steps)
     # Jitter step size; ref: https://github.com/stan-dev/stan/blob/1bb054027b01326e66ec610e95ef9b2a60aa6bec/src/stan/mcmc/hmc/base_hmc.hpp#L177-L178
-    iszero(lf.jitter) || (ϵ *= (1 + lf.jitter * (2 * rand() - 1)))
+    !iszero(lf.jitter) && (ϵ *= (1 + lf.jitter * (2 * rand() - 1)))
 
     @unpack value, gradient = ∂H∂θ(h, θ)
     for i = 1:n_steps
         # Tempering; `ceil` includes mid if `n_steps` is odd, e.g. `<= ceil(5 / 2)` => `<= 3` 
-        r = i <= ceil(Int, n_steps / 2) ? r * sqrtα : r / sqrtα     
+        !isone(sqrtα) && (r = i <= ceil(Int, n_steps / 2) ? r * sqrtα : r / sqrtα)
         r = r - ϵ / 2 * gradient    # take a half leapfrog step for momentum variable
         ∇r = ∂H∂r(h, r)
         θ = θ + ϵ * ∇r  # take a full leapfrog step for position variable
         @unpack value, gradient = ∂H∂θ(h, θ)
         r = r - ϵ / 2 * gradient    # take a half leapfrog step for momentum variable
         # Tempering; `floor` excludes mid if `n_steps` is odd, e.g. `<= floor(5 / 2)` => `<= 2` 
-        r = i <= floor(Int, n_steps / 2) ? r * sqrtα : r / sqrtα
+        !isone(sqrtα) && (r = i <= floor(Int, n_steps / 2) ? r * sqrtα : r / sqrtα)
         # Create a new phase point by caching the logdensity and gradient
         z = phasepoint(h, θ, r; ℓπ=DualValue(value, gradient))
         !isfinite(z) && break
