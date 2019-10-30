e2431ff45ceebf0a1bba97f6d227591488127756
Kai Xu <xukai921110@gmail.com>
2019-10-30 13:45:46 +0000

    improve tests for hmc_mat

--- test/hmc_mat.jl
+++ test/hmc_mat.jl
@@ -4,10 +4,12 @@ include("common.jl")
 
 @testset "Matrix mode" begin
     n_chains_max = 20
-    θ_init = [randn(D, n_chains) for n_chains in 1:n_chains_max]
+    n_chains_list = collect(1:n_chains_max)
+    θ_init_list = [randn(D, n_chains) for n_chains in n_chains_list]
     ϵ = 0.1
     lf = Leapfrog(ϵ)
-    lf5 = Leapfrog(fill(ϵ, 5))
+    i_test = 5
+    lfi = Leapfrog(fill(ϵ, i_test))
     n_steps = 20
     n_samples = 10_000
     n_adapts = 2_000
@@ -17,30 +19,32 @@ include("common.jl")
         DiagEuclideanMetric,
         # DenseEuclideanMetric  # not supported at the moment
     ], τ in [
-        StaticTrajectory(lf5, n_steps),
+        StaticTrajectory(lfi, n_steps),
         HMCDA(lf, ϵ * n_steps)
     ]
-        metric = metricT((D, 5))
+
+        n_chains = n_chains_list[i_test]
+        metric = metricT((D, n_chains))
         h = Hamiltonian(metric, ℓπ, ∂ℓπ∂θ)
-        samples, stats = sample(h, τ, θ_init[5], n_samples; verbose=false)
-        @test mean(samples) ≈ zeros(D, 5) atol=RNDATOL
+        samples, stats = sample(h, τ, θ_init_list[i_test], n_samples; verbose=false)
+        @test mean(samples) ≈ zeros(D, n_chains) atol=RNDATOL * n_chains
 
         for adaptor in [
             Preconditioner(metric),
-            NesterovDualAveraging(0.8, lf5.ϵ),
+            NesterovDualAveraging(0.8, lfi.ϵ),
             NaiveHMCAdaptor(
                 Preconditioner(metric),
-                NesterovDualAveraging(0.8, lf5.ϵ),
+                NesterovDualAveraging(0.8, lfi.ϵ),
             ),
             StanHMCAdaptor(
                 n_adapts,
                 Preconditioner(metric),
-                NesterovDualAveraging(0.8, lf5.ϵ),
+                NesterovDualAveraging(0.8, lfi.ϵ),
             ),
         ]
             τ isa HMCDA && continue
-            samples, stats = sample(h, τ, θ_init[5], n_samples, adaptor, n_adapts; verbose=false, progress=false)
-            @test mean(samples) ≈ zeros(D, 5) atol=RNDATOL
+            samples, stats = sample(h, τ, θ_init_list[i_test], n_samples, adaptor, n_adapts; verbose=false, progress=false)
+            @test mean(samples) ≈ zeros(D, n_chains) atol=RNDATOL * n_chains
         end
     end
     @info "Adaptation tests for HMCDA with NesterovDualAveraging are skipped"
@@ -50,19 +54,19 @@ include("common.jl")
         τ = StaticTrajectory(lf, n_steps)
 
         time_mat = Vector{Float64}(undef, n_chains_max)
-        for (i, n_chains) in enumerate(1:n_chains_max)
+        for (i, n_chains) in enumerate(n_chains_list)
             h = Hamiltonian(metricT((D, n_chains)), ℓπ, ∂ℓπ∂θ)
-            t = @elapsed samples, stats = sample(h, τ, θ_init[i], n_samples; verbose=false)
+            t = @elapsed samples, stats = sample(h, τ, θ_init_list[i], n_samples; verbose=false)
             time_mat[i] = t
         end
 
         # Time for multiple runs of single chain
         time_seperate = Vector{Float64}(undef, n_chains_max)
 
-        for (i, n_chains) in enumerate(1:n_chains_max)
+        for (i, n_chains) in enumerate(n_chains_list)
             t = @elapsed for j in 1:n_chains
                 h = Hamiltonian(metricT(D), ℓπ, ∂ℓπ∂θ)
-                samples, stats = sample(h, τ, θ_init[i][:,j], n_samples; verbose=false)
+                samples, stats = sample(h, τ, θ_init_list[i][:,j], n_samples; verbose=false)
             end
             time_seperate[i] = t
         end
@@ -76,7 +80,7 @@ include("common.jl")
             xlabel="Num of chains",
             ylabel="Time (s)"
         )
-        lineplot!(fig, collect(1:n_chains_max), time_seperate; color=:blue, name="seperate")
+        lineplot!(fig, collect(n_chains_list), time_seperate; color=:blue, name="seperate")
         println(); show(fig); println(); println()
     end
 end
