08e1a020b86ae9034bfbd280697cb53107be8509
Kai Xu <xukai921110@gmail.com>
2019-11-03 18:03:33 +0000

    introduce the missing factor

--- src/diagnosis.jl
+++ src/diagnosis.jl
@@ -1,3 +1,3 @@
-function EBFMI(Es::AbstractVector{<:Real})
-    return sum(i -> (Es[i+1] - Es[i])^2, 1:length(Es)-1) / var(Es)
+function EBFMI(Es::AbstractVector{<:AbstractFloat})
+    return mean(i -> (Es[i+1] - Es[i])^2, 1:length(Es)-1) / var(Es)
 end
