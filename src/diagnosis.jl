906b336efcc60dd15fb1e6f05da27dff2efde1de
Kai Xu <xukai921110@gmail.com>
2019-12-05 23:13:18 -0500

    fix merge issue (#141)

--- src/diagnosis.jl
+++ src/diagnosis.jl
@@ -1,3 +1,3 @@
 function EBFMI(Es::AbstractVector{<:AbstractScalarOrVec{<:AbstractFloat}})
-    return sum(i -> (Es[i+1] - Es[i]) .^ 2, 1:length(Es)-1) ./ var(Es)
+    return mean(i -> (Es[i+1] - Es[i]) .^ 2, 1:length(Es)-1) ./ var(Es)
 end
