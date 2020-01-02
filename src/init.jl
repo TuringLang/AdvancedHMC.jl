258dbcf4e387111c21e3d108470348525febf48f
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2020-01-02 19:18:13 +0200

    Renamed a few variables

--- src/init.jl
+++ src/init.jl
@@ -5,11 +5,11 @@ function __init__()
 
         struct DiffEqIntegrator{T<:AbstractScalarOrVec{<:AbstractFloat}, DiffEqSolver} <: AbstractLeapfrog{T}
             ϵ::T
-            algorithm::DiffEqSolver
+            solver::DiffEqSolver
         end
 
         function step(
-            lf::DiffEqIntegrator,
+            integrator::DiffEqIntegrator,
             h::Hamiltonian,
             z::P,
             n_steps::Int=1;
@@ -26,14 +26,15 @@ function __init__()
 			f1(v, u, p, t) = -∂H∂θ(h, u).gradient
 			f2(v, u, p, t) = ∂H∂r(h, v)
 
-            ϵ = fwd ? step_size(lf) : -step_size(lf)
+            ϵ = fwd ? step_size(integrator) : -step_size(integrator)
             tspan = (0.0, sign(n_steps))
-            prob = DynamicalODEProblem(f1, f2, v0, u0, tspan)
-            integrator = init(prob, lf.alg, save_everystep=false, save_start=false, save_end=false, dt=ϵ)
+            problem = DynamicalODEProblem(f1, f2, v0, u0, tspan)
+            diffeq_integrator = init(problem, integrator.solver, save_everystep=false, save_start=false, save_end=false, dt=ϵ)
 
             for i in 1:abs(n_steps)
-                step!(integrator)
-                z = phasepoint(h, integrator.u.x[2], integrator.u.x[1])
+                step!(diffeq_integrator)
+                solution = diffeq_integrator.u.x  # (r, θ) at the proposed step
+                z = phasepoint(h, solution[2], solution[1])
                 !isfinite(z) && break
                 if res isa Vector
                     res[i] = z
