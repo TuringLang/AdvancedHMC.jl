2fccc2ab6e97f656024645632431a8b12508774e
Ivan Yashchuk <IvanYashchuk@users.noreply.github.com>
2020-01-03 00:48:54 +0500

    Add DifferentialEquations.jl integrators (#154)
    * make DiffEqInt runnable
    
    * Fixed error, two functions were with same name
    
    both OrdinaryDiffEq and Adaptation export "initialize!"
    
    * HMC equations are now correct
    
    * Fixed warning that OrdinaryDiffEq not being in dependencies
    
    * Now Parameters.reconstruct works with DiffEqIntegrator
    
    * Renamed a few variables
    
    * Fixed indentation formatting
    
    * Fixed DiffEqIntegrator instantiation in test
    
    Co-authored-by: Kai Xu <xukai921110@gmail.com>

--- src/init.jl
+++ src/init.jl
@@ -1,40 +1,51 @@
 function __init__()
     @require OrdinaryDiffEq = "1dea7af3-3e70-54e6-95c3-0bf5283fa5ed" begin
 
-        struct DiffEqIntegrator{A}
-          alg::A
-          DiffEqIntegrator(alg::T=VelocityVerlet()) where T = new{T}(alg)
+        using .OrdinaryDiffEq
+
+        struct DiffEqIntegrator{T<:AbstractScalarOrVec{<:AbstractFloat}, DiffEqSolver} <: AbstractLeapfrog{T}
+            ϵ::T
+            solver::DiffEqSolver
         end
 
         function step(
-            lf::DiffEqIntegrator,
+            integrator::DiffEqIntegrator,
             h::Hamiltonian,
             z::P,
             n_steps::Int=1;
             fwd::Bool=n_steps > 0,  # simulate hamiltonian backward when n_steps < 0
-            res::Union{Vector{P}, P}=z) where P
+            res::Union{Vector{P}, P}=z) where {P<:PhasePoint}
 
             @unpack θ, r = z
-            u0 = θ
-            v0 = r
-            function f1(v,u,p,t)
-              ∂H∂r(h, θ)[2]
-            end
-            function f2(v,u,p,t)
-              ∂H∂θ(h, θ)[2]
-            end
-            ϵ = fwd ? step_size(lf) : -step_size(lf)
-            tspan = (0.0,sign(n_steps)*1.0) # Ignored and only used for direction
-            prob = DynamicalODEProblem(f1,f2,v0,u0,tspan)
-            integrator = init(prob,lf.alg,save_everystep=false,save_start=false,
-                              save_end=false,dt=ϵ)
+
+            # For DynamicalODEProblem `u` is `θ` and `v` is `r`
+            # f1 is dr/dt RHS function
+            # f2 is dθ/dt RHS function
+            v0, u0 = r, θ
+
+            f1(v, u, p, t) = -∂H∂θ(h, u).gradient
+            f2(v, u, p, t) = ∂H∂r(h, v)
+
+            ϵ = fwd ? step_size(integrator) : -step_size(integrator)
+            tspan = (0.0, sign(n_steps))
+            problem = DynamicalODEProblem(f1, f2, v0, u0, tspan)
+            diffeq_integrator = init(problem, integrator.solver, save_everystep=false, save_start=false, save_end=false, dt=ϵ)
 
             for i in 1:abs(n_steps)
-              step!(integrator)
+                step!(diffeq_integrator)
+                solution = diffeq_integrator.u.x  # (r, θ) at the proposed step
+                z = phasepoint(h, solution[2], solution[1])
+                !isfinite(z) && break
+                if res isa Vector
+                    res[i] = z
+                else
+                    res = z
+                end
             end
-
-            integrator.u.x[2]
+            return res
         end
+
+        export DiffEqIntegrator
 
     end
 end
