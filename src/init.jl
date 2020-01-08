71be05ceeed3a7958c79607cdc3cbd618bfab85e
Vaibhav Kumar Dixit <vaibhavyashdixit@gmail.com>
2020-01-09 03:51:03 +0530

    Use import for OrdinaryDiffEq to prevent initialize! clash (#160)

--- src/init.jl
+++ src/init.jl
@@ -1,7 +1,7 @@
 function __init__()
     @require OrdinaryDiffEq = "1dea7af3-3e70-54e6-95c3-0bf5283fa5ed" begin
 
-        using .OrdinaryDiffEq
+        import .OrdinaryDiffEq
 
         struct DiffEqIntegrator{T<:AbstractScalarOrVec{<:AbstractFloat}, DiffEqSolver} <: AbstractLeapfrog{T}
             ϵ::T
@@ -28,11 +28,11 @@ function __init__()
 
             ϵ = fwd ? step_size(integrator) : -step_size(integrator)
             tspan = (0.0, sign(n_steps))
-            problem = DynamicalODEProblem(f1, f2, v0, u0, tspan)
-            diffeq_integrator = init(problem, integrator.solver, save_everystep=false, save_start=false, save_end=false, dt=ϵ)
+            problem = OrdinaryDiffEq.DynamicalODEProblem(f1, f2, v0, u0, tspan)
+            diffeq_integrator = OrdinaryDiffEq.init(problem, integrator.solver, save_everystep=false, save_start=false, save_end=false, dt=ϵ)
 
             for i in 1:abs(n_steps)
-                step!(diffeq_integrator)
+                OrdinaryDiffEq.step!(diffeq_integrator)
                 solution = diffeq_integrator.u.x  # (r, θ) at the proposed step
                 z = phasepoint(h, solution[2], solution[1])
                 !isfinite(z) && break
