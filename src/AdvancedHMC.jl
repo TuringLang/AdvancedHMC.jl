726031ca86b4017768f23ed8f375b2ef840b3b3f
Kai Xu <xukai921110@gmail.com>
2020-03-04 19:42:54 +0000

    remove lazy arrays

--- src/AdvancedHMC.jl
+++ src/AdvancedHMC.jl
@@ -5,9 +5,8 @@ const DEBUG = Bool(parse(Int, get(ENV, "DEBUG_AHMC", "0")))
 using Statistics: mean, var, middle
 using LinearAlgebra: Symmetric, UpperTriangular, mul!, ldiv!, dot, I, diag, cholesky
 using StatsFuns: logaddexp
-using LazyArrays: BroadcastArray
 using Random: GLOBAL_RNG, AbstractRNG
-using ProgressMeter
+using ProgressMeter: ProgressMeter
 using Parameters: @unpack, reconstruct
 using ArgCheck: @argcheck
 
