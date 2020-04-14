d086fc5d72a63ec808d488317fff1e9f45b3bc57
Kai Xu <xukai921110@gmail.com>
2020-04-14 13:23:20 +0100

    fix typo in deprecate code

--- src/AdvancedHMC.jl
+++ src/AdvancedHMC.jl
@@ -70,7 +70,7 @@ MassMatrixAdaptor(
 
 @deprecate StanHMCAdaptor(n_adapts, pc, ssa) initialize!(StanHMCAdaptor(pc, ssa), n_adapts)
 @deprecate NesterovDualAveraging(δ, i::AbstractIntegrator) StepSizeAdaptor(δ, i)
-@deprecate Preconditioner(m::AbstractMetric) MassMatrixAdaptor(m)
+@deprecate Preconditioner(args...) MassMatrixAdaptor(args...)
 
 export StepSizeAdaptor, NesterovDualAveraging, 
        MassMatrixAdaptor, UnitMassMatrix, WelfordVar, WelfordCov, 
