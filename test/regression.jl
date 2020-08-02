9be4d482c2388ca9b8bbe54dacaa713ea1234c94
Kai Xu <xukai921110@gmail.com>
2020-08-02 11:15:58 +0100

    remove comments from regression.jl

--- test/regression.jl
+++ test/regression.jl
@@ -1,54 +1,42 @@
 using Test, Random, BSON, AdvancedHMC, Distributions, ForwardDiff
 
 function simulate_and_compare(hmc_variant)
+    Random.seed!(1)
 
-Random.seed!(1)
+    dim = 10; θ₀ = rand(dim)
 
-# Set parameter dimensionality and initial parameter value
-dim = 10; θ₀ = rand(dim)
+    ℓπ(θ) = logpdf(MvNormal(zeros(dim), ones(dim)), θ)
 
-# Define the target distribution
-ℓπ(θ) = logpdf(MvNormal(zeros(dim), ones(dim)), θ)
+    n_samples, n_adapts = 2_000, 1_000
 
-# Set the number of samples to draw and iterations for warmup
-n_samples, n_adapts = 2_000, 1_000
+    metric = DiagEuclideanMetric(dim)
+    hamiltonian = Hamiltonian(metric, ℓπ, ForwardDiff)
 
-# Define a Hamiltonian system
-metric = DiagEuclideanMetric(dim)
-hamiltonian = Hamiltonian(metric, ℓπ, ForwardDiff)
+    ϵ₀ = find_good_stepsize(hamiltonian, θ₀)
+    integrator = Leapfrog(ϵ₀)
 
-# Define a leapfrog solver, with initial step size chosen heuristically
-ϵ₀ = find_good_stepsize(hamiltonian, θ₀)
-integrator = Leapfrog(ϵ₀)
+    κ = if hmc_variant == "hmc_mh"
+        HMCKernel(FullRefreshment(), Trajectory(integrator, FixedNSteps(10)), MetropolisTS)
+    elseif hmc_variant == "hmc_multi"
+        HMCKernel(FullRefreshment(), Trajectory(integrator, FixedNSteps(10)), MultinomialTS)
+    elseif hmc_variant == "nuts_slice"
+        HMCKernel(FullRefreshment(), Trajectory(integrator, NoUTurn()), SliceTS)
+    elseif hmc_variant == "nuts_multi"
+        HMCKernel(FullRefreshment(), Trajectory(integrator, NoUTurn()), MultinomialTS)
+    end
 
-kernel =
-if hmc_variant == "hmc_mh"
-    HMCKernel(FullRefreshment(), Trajectory(integrator, FixedNSteps(10)), MetropolisTS)
-elseif hmc_variant == "hmc_multi"
-    HMCKernel(FullRefreshment(), Trajectory(integrator, FixedNSteps(10)), MultinomialTS)
-elseif hmc_variant == "nuts_slice"
-    HMCKernel(FullRefreshment(), Trajectory(integrator, NoUTurn()), SliceTS)
-elseif hmc_variant == "nuts_multi"
-    HMCKernel(FullRefreshment(), Trajectory(integrator, NoUTurn()), MultinomialTS)
+    adaptor = StanHMCAdaptor(MassMatrixAdaptor(metric), StepSizeAdaptor(0.8, ϵ₀))
+
+    samples, stats = sample(hamiltonian, κ, θ₀, n_samples, adaptor, n_adapts; progress=true)
+
+    old = BSON.load("$(@__DIR__)/regression/ef6de39/$hmc_variant.bson")
+
+    @test samples == old[:samples]
+    @test stats == old[:stats]
 end
 
-adaptor = StanHMCAdaptor(MassMatrixAdaptor(metric), StepSizeAdaptor(0.8, ϵ₀))
-
-# Run the sampler to draw samples from the specified Gaussian, where
-#   - `samples` will store the samples
-#   - `stats` will store diagnostic statistics for each sample
-samples, stats = sample(hamiltonian, kernel, θ₀, n_samples, adaptor, n_adapts; progress=true)
-
-old = BSON.load("$(@__DIR__)/regression/ef6de39/$hmc_variant.bson")
-
-@test samples == old[:samples]
-@test stats == old[:stats]
-
-end # function
-
 @testset "Regression" begin
-    simulate_and_compare("hmc_mh")
-    simulate_and_compare("hmc_multi")
-    simulate_and_compare("nuts_slice")
-    simulate_and_compare("nuts_multi")
+    for hmc_variant in ["hmc_mh", "hmc_multi", "nuts_slice", "nuts_multi"]
+        simulate_and_compare(hmc_variant)
+    end
 end
