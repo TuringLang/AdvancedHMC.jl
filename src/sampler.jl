b6ad39cb0fc83f11523b0b042aa278b5b5d7a62a
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-04-08 15:51:06 +0200

    replicate logging behavior of current AHMC when using AbstractMCMC

--- src/sampler.jl
+++ src/sampler.jl
@@ -17,7 +17,7 @@ function reconstruct(
     return reconstruct(τ, integrator=integrator)
 end
 
-reconstruct(κ::AbstractHMCKernel, adaptor::AbstractAdaptor) = 
+reconstruct(κ::AbstractMCMCKernel, adaptor::AbstractAdaptor) =
     reconstruct(κ, τ=reconstruct(κ.τ, adaptor))
 
 function resize(h::Hamiltonian, θ::AbstractVecOrMat{T}) where {T<:AbstractFloat}
@@ -58,7 +58,7 @@ end
 
 Adaptation.adapt!(
     h::Hamiltonian,
-    κ::AbstractHMCKernel,
+    κ::AbstractMCMCKernel,
     adaptor::Adaptation.NoAdaptation,
     i::Int,
     n_adapts::Int,
@@ -68,7 +68,7 @@ Adaptation.adapt!(
 
 function Adaptation.adapt!(
     h::Hamiltonian,
-    κ::AbstractHMCKernel,
+    κ::AbstractMCMCKernel,
     adaptor::AbstractAdaptor,
     i::Int,
     n_adapts::Int,
@@ -105,7 +105,7 @@ simple_pm_next!(pm, stat::NamedTuple) = ProgressMeter.next!(pm)
 
 sample(
     h::Hamiltonian,
-    κ::AbstractHMCKernel,
+    κ::AbstractMCMCKernel,
     θ::AbstractVecOrMat{<:AbstractFloat},
     n_samples::Int,
     adaptor::AbstractAdaptor=NoAdaptation(),
@@ -132,7 +132,7 @@ sample(
     sample(
         rng::AbstractRNG,
         h::Hamiltonian,
-        κ::AbstractHMCKernel,
+        κ::AbstractMCMCKernel,
         θ::AbstractVecOrMat{T},
         n_samples::Int,
         adaptor::AbstractAdaptor=NoAdaptation(),
@@ -227,7 +227,7 @@ end
 struct HMCState{
     TTrans<:Transition,
     TMetric<:AbstractMetric,
-    TKernel<:AbstractHMCKernel,
+    TKernel<:AbstractMCMCKernel,
     TAdapt<:Adaptation.AbstractAdaptor
 }
     i::Int
@@ -255,10 +255,24 @@ function AbstractMCMC.sample(
     metric::AbstractMetric,
     adaptor::AbstractAdaptor,
     N::Integer;
+    progress = true,
+    verbose = false,
+    callback = nothing,
     kwargs...
 )
     sampler = HMCSampler(kernel, metric, adaptor)
-    return AbstractMCMC.mcmcsample(rng, model, sampler, N; kwargs...)
+    if callback === nothing
+        callback = HMCProgressCallback(N, progress = progress, verbose = verbose)
+        progress = false # don't use AMCMC's progress-funtionality
+    end
+
+    return AbstractMCMC.mcmcsample(
+        rng, model, sampler, N;
+        progress = progress,
+        verbose = verbose,
+        callback = callback,
+        kwargs...
+    )
 end
 
 function AbstractMCMC.sample(
@@ -286,10 +300,24 @@ function AbstractMCMC.sample(
     parallel::AbstractMCMC.AbstractMCMCParallel,
     N::Integer,
     nchains::Integer;
+    progress = true,
+    verbose = false,
+    callback = nothing,
     kwargs...
 )
     sampler = HMCSampler(kernel, metric, adaptor)
-    return AbstractMCMC.mcmcsample(rng, model, sampler, parallel, N, nchains; kwargs...)
+    if callback === nothing
+        callback = HMCProgressCallback(N, progress = progress, verbose = verbose)
+        progress = false # don't use AMCMC's progress-funtionality
+    end
+
+    return AbstractMCMC.mcmcsample(
+        rng, model, sampler, parallel, N, nchains;
+        progress = progress,
+        verbose = verbose,
+        callback = callback,
+        kwargs...
+    )
 end
 
 function AbstractMCMC.step(
@@ -326,7 +354,6 @@ function AbstractMCMC.step(
     spl::HMCSampler,
     state::HMCState;
     nadapts::Int = 0,
-    verbose::Bool = true,
     kwargs...
 )
     # Get step size
@@ -361,28 +388,41 @@ end
 ################
 ### Callback ###
 ################
+struct HMCProgressCallback{P}
+    pm::P
+    progress::Bool
+    verbose::Bool
+end
 
-# TODO: add as a default once AbstractMCMC.jl has been updated
-struct HMCCallback end
+function HMCProgressCallback(n_samples; progress=true, verbose=false)
+    pm = progress ? ProgressMeter.Progress(n_samples, desc="Sampling", barlen=31) : nothing
+    HMCProgressCallback(pm, progress, verbose)
+end
 
-function (cb::HMCCallback)(
-    rng, model, t, spl, state, i;
-    progress, verbose, kwargs...
+function (cb::HMCProgressCallback)(
+    rng, model, spl, t, state, i;
+    nadapts = 0,
+    kwargs...
 )
-    h = model.hamiltonian
-    adaptor = spl.adaptor
-    κ = spl.kernel
-    isadapted = t.stat.is_adapt
+    progress = cb.progress
+    verbose = cb.verbose
+    pm = cb.pm
+    
+    metric = state.metric
+    adaptor = state.adaptor
+    κ = state.κ
+    tstat = t.stat
+    isadapted = tstat.is_adapt
 
     # Update progress meter
     if progress
         # Do include current iteration and mass matrix
         pm_next!(
             pm,
-            (iterations=i, tstat..., mass_matrix=h.metric)
+            (iterations=i, tstat..., mass_matrix=metric)
         )
         # Report finish of adapation
-    elseif verbose && isadapted && i == n_adapts
-        @info "Finished $n_adapts adapation steps" adaptor κ.τ.integrator h.metric
+    elseif verbose && isadapted && i == nadapts
+        @info "Finished $nadapts adapation steps" adaptor κ.τ.integrator metric
     end
 end
