5158a7be307caa8c894b7a45434bc966139daa11
Kai Xu <5985769+xukai92@users.noreply.github.com>
2021-06-10 14:47:05 +0100

    Update hamiltonian.jl

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -45,6 +45,7 @@ struct PhasePoint{T<:AbstractVecOrMat{<:AbstractFloat}, V<:DualValue}
         @argcheck length(θ) == length(r) == length(ℓπ.gradient) == length(ℓπ.gradient)
         if any(isfinite.((θ, r, ℓπ, ℓκ)) .== false)
             @warn "The current proposal will be rejected due to numerical error(s)." isfinite.((θ, r, ℓπ, ℓκ))
+            # NOTE eltype has to be inlined to avoid type stability issue; see #267
             ℓπ = DualValue(
                 map(v -> isfinite(v) ? v : -eltype(T)(Inf), ℓπ.value), 
                 ℓπ.gradient
