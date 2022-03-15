09b3f4a4541dd70692f0ab1dc9e638c2efa7fed4
Kai Xu <5985769+xukai92@users.noreply.github.com>
2022-03-15 19:17:46 +0000

    Documentation setup (#286)
    * initial doc setup
    
    * initial doc setup
    
    * instantiate before build
    
    * add TuringWeb explictly
    
    * fix build script
    
    * update build script
    
    * update build script
    
    * update build script
    
    * use README.md as index page
    
    Co-authored-by: Kai Xu <contact@xuk.ai>

---
+++ docs/make.jl
@@ -0,0 +1,18 @@
+using Pkg
+Pkg.update("TuringWeb")
+
+using Documenter, TuringWeb
+using AdvancedHMC
+
+# cp(joinpath(@__DIR__, "../README.md"), joinpath(@__DIR__, "src/index.md"))
+
+makedocs(
+    sitename = "AdvancedHMC",
+    format = Documenter.HTML(),
+    modules = [AdvancedHMC]
+)
+
+deploydocs(
+    repo = "github.com/TuringLang/AdvancedHMC.jl.git",
+    push_preview = true, # allow PR to deploy
+)
