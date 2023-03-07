c91a07ef56bd659330073f6133619d1673cec554
Kai Xu <xuk@amazon.com>
2023-03-07 16:45:27 +0000

    fix: flip sign in dHdx

--- research/tests/riemannian_hmc.jl
+++ research/tests/riemannian_hmc.jl
@@ -1,114 +1,76 @@
-using InteractiveUtils, Pkg, Revise
-versioninfo(); Pkg.status()
+using ReTest, AdvancedHMC
 
-import PyPlot; const plt = PyPlot
-using Random, LinearAlgebra, Statistics, ReverseDiff, VecTargets, AdvancedHMC
+include("../src/riemannian_hmc.jl")
+include("../src/riemannian_hmc-sampler.jl")
 
-# Fisher information metric
-function gen_∂H∂x(func, x; f=identity)
-    hess = VecTargets.gen_hess(func, ReverseDiff.track.(x))
-    # QUES What's the best output format of this function?
-    return x -> ReverseDiff.jacobian(x -> f(hess(x)[3]), x) # default output shape [∂H∂x₁; ∂H∂x₂; ...]
-end
+using FiniteDiff: finite_difference_gradient, finite_difference_hessian, finite_difference_jacobian
+using Distributions: MvNormal
+using AdvancedHMC: neg_energy, energy
 
-function reshape_H(H)
-    d = size(H, 2)
-    return cat((H[(i-1)*d+1:i*d,:] for i in 1:d)...; dims=3)
-end
+# Taken from https://github.com/JuliaDiff/FiniteDiff.jl/blob/master/test/finitedifftests.jl
+δ(a, b) = maximum(abs.(a - b))
 
-function sample_target(hps; rng=MersenneTwister(1110))    
-    target = hps.target == :gaussian  ? HighDimGaussian(2) :
-             hps.target == :banana    ? Banana() :
-             hps.target == :funnel    ? Funnel() :
-             hps.target == :funnel101 ? Funnel(101) :
-             hps.target == :spiral    ? Spiral(8, 0.1) :
-             hps.target == :mogs      ? TwoDimGaussianMixtures() :
-             @error "Unknown target $(hps.target)"
-    D = dim(target)
-    initial_θ = rand(rng, D)
-    
-    ℓπ = x -> logpdf(target, x)
-    _∂ℓπ∂θ = gen_logpdf_grad(target, initial_θ)
-    ∂ℓπ∂θ = x -> copy.(_∂ℓπ∂θ(x))
-    
-    neg_ℓπ = x -> -logpdf(target, x)
-    _hess_func = VecTargets.gen_hess(neg_ℓπ, initial_θ) # x -> (value, gradient, hessian)
-    hess_func = x -> copy.(_hess_func(x))
-    
-    G = x -> begin
-        H = hess_func(x)[3] + hps.λ * I
-        any(.!(isfinite.(H))) ? diagm(ones(length(x))) : H
+@testset "Riemannian" begin
+
+    hps = (; λ=1e-2, α=20.0, ϵ=0.1, n=6, L=8)
+
+    @testset "$(nameof(typeof(target)))" for target in [HighDimGaussian(2), Funnel()]
+        rng = MersenneTwister(1110)
+
+        D = dim(target) # ==2 for this test
+        initial_θ = rand(rng, D)
+
+        ℓπ = x -> logpdf(target, x)
+        neg_ℓπ = x -> -logpdf(target, x)
+
+        _∂ℓπ∂θ = gen_logpdf_grad(target, initial_θ)
+        ∂ℓπ∂θ = x -> copy.(_∂ℓπ∂θ(x))
+        
+        _hess_func = VecTargets.gen_hess(neg_ℓπ, initial_θ) # x -> (value, gradient, hessian)
+        hess_func = x -> copy.(_hess_func(x))
+
+        G = x -> begin
+            H = hess_func(x)[3] + hps.λ * I
+            any(.!(isfinite.(H))) ? diagm(ones(length(x))) : H
+        end
+        _∂G∂θ = gen_∂H∂x(neg_ℓπ, initial_θ) # size==(4, 2)
+        ∂G∂θ = x -> reshape_∂H∂x(copy(_∂G∂θ(x))) # size==(2, 2, 2)
+
+        x = randn(rng, 2)
+        r = randn(rng, 2)
+
+        @testset "Autodiff" begin
+            @test δ(finite_difference_gradient(ℓπ, x), ∂ℓπ∂θ(x)[end]) < 1e-4
+            @test δ(finite_difference_hessian(neg_ℓπ, x), hess_func(x)[end]) < 1e-4
+            # finite_difference_jacobian returns shape of (4, 2)
+            @test δ(finite_difference_jacobian(G, x), _∂G∂θ(x)) < 1e-4
+        end
+
+        @testset "$(nameof(typeof(hessmap)))" for hessmap in [IdentityMap(), SoftAbsMap(hps.α)]
+            metric = DenseRiemannianMetric((D,), G, ∂G∂θ, hessmap)
+            kinetic = GaussianKinetic()
+            hamiltonian = Hamiltonian(metric, kinetic, ℓπ, ∂ℓπ∂θ)
+
+            if hessmap isa SoftAbsMap # only test kinetic energy for SoftAbsMap as that of IdentityMap can be non-PD
+                @testset "Kinetic energy" begin
+                    Σ = hamiltonian.metric.map(hamiltonian.metric.G(x))
+                    @test neg_energy(hamiltonian, r, x) ≈ logpdf(MvNormal(zeros(D), Σ), r)
+                end
+            end
+
+            H_func = (x, r) -> energy(hamiltonian, r, x) + energy(hamiltonian, x)
+            Hx = x -> H_func(x, r)
+            Hr = r -> H_func(x, r)
+
+            @testset "∂H∂θ" begin
+                @test δ(finite_difference_gradient(Hx, x), ∂H∂θ(hamiltonian, x, r).gradient) < 1e-4
+            end
+
+            @testset "∂H∂r" begin
+                @test δ(finite_difference_gradient(Hr, r), ∂H∂r(hamiltonian, x, r)) < 1e-4
+            end
+        end
+
     end
-    _∂G∂θ = gen_∂H∂x(neg_ℓπ, initial_θ)
-    ∂G∂θ = x -> reshape_H(copy(_∂G∂θ(x)))
 
-    metric = hps.metric == :dense_euclidean          ? DenseEuclideanMetric(D) :
-             hps.metric == :dense_riemannian         ? DenseRiemannianMetric((D,), G, ∂G∂θ) :
-             hps.metric == :dense_riemannian_softabs ? DenseRiemannianMetric((D,), G, ∂G∂θ, SoftAbsMap(hps.α)) :
-             @error "Unknown metric $(hps.metric)"
-    kinetic = GaussianKinetic()
-    hamiltonian = Hamiltonian(metric, kinetic, ℓπ, ∂ℓπ∂θ)
-
-    TS = EndPointTS
-    
-    integrator = hps.integrator == :lf  ? Leapfrog(hps.ϵ) :
-                 hps.integrator == :glf ? GeneralizedLeapfrog(hps.ϵ, hps.n) :
-                 @error "Unknown integrator $(hps.integrator)"
-
-    tc = FixedNSteps(hps.L)
-    
-    proposal = HMCKernel(Trajectory{TS}(integrator, tc))
-
-    samples, stats = sample(
-        rng, hamiltonian, proposal, initial_θ, hps.n_samples; progress=false, verbose=true
-    )
-    
-    return (; target, hamiltonian, samples, stats)
-end
-
-hps = (; target=:gaussian, n_samples=2_000, metric=:dense_euclidean, λ=1e-2, α=20.0, integrator=:lf, ϵ=0.1, n=6, L=8)
-retval = sample_target((; hps...))
-
-@info "Samples" mean(retval.samples) var(retval.samples)
-
-retval = sample_target((; hps..., integrator=:glf))
-
-@info "Samples" mean(retval.samples) var(retval.samples)
-
-@time retval = sample_target((; hps..., metric=:dense_riemannian, integrator=:glf))
-
-@info "Samples" mean(retval.samples) var(retval.samples)
-
-using Logging: NullLogger, with_logger
-
-@time retval = with_logger(NullLogger()) do 
-    sample_target((; hps..., target=:funnel, metric=:dense_riemannian, integrator=:glf))
-end
-
-@info "Average acceptance ratio" mean(map(s -> s.is_accept, retval.stats))
-
-let (fig, ax) = plt.subplots()
-    plt.close(fig)
-    
-    ax.scatter(map(s -> s[1], retval.samples), map(s -> s[2], retval.samples))
-    
-    fig
-end
-
-@time retval = sample_target((; hps..., metric=:dense_riemannian_softabs, integrator=:glf))
-
-@info "Samples" mean(retval.samples) var(retval.samples)
-
-@time retval = with_logger(NullLogger()) do 
-    sample_target((; hps..., target=:funnel, metric=:dense_riemannian_softabs, integrator=:glf)) # ϵ=0.1
-end
-
-@info "Average acceptance ratio" mean(map(s -> s.is_accept, retval.stats))
-
-let (fig, ax) = plt.subplots()
-    plt.close(fig)
-    
-    ax.scatter(map(s -> s[1], retval.samples), map(s -> s[2], retval.samples))
-    
-    fig
 end
--- research/tests/runtests.jl
+++ research/tests/runtests.jl
@@ -1,10 +1,12 @@
-using ReTest
+using Comonicon, ReTest
 
-# include the source code for relativistic HMC
+# include the source code for experimental HMC
 include("../src/relativistic_hmc.jl")
+include("../src/riemannian_hmc.jl")
 
-# include the tests for relativistic HMC
+# include the tests for experimental HMC
 include("relativistic_hmc.jl")
+include("riemannian_hmc.jl")
 
 @main function runtests(patterns...; dry::Bool=false)
     retest(patterns...; dry=dry, verbose=Inf)
