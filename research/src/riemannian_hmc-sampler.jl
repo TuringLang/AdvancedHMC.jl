46c544e27b2502512e0f2530bcfd887fb61d4020
Kai Xu <xuk@amazon.com>
2023-03-27 04:10:06 +0000

    perf: use ForwardDiff to compute ∂G∂θ

--- research/src/riemannian_hmc-sampler.jl
+++ research/src/riemannian_hmc-sampler.jl
@@ -1,11 +1,33 @@
-using Random, LinearAlgebra, ReverseDiff, VecTargets
+using Random, LinearAlgebra, ReverseDiff, ForwardDiff, VecTargets
 
 # Fisher information metric
-function gen_∂G∂θ(Vfunc, x; f=identity)
-    Hfunc = VecTargets.gen_hess(Vfunc, ReverseDiff.track.(x))
+function gen_∂G∂θ_rev(Vfunc, x; f=identity)
+    _Hfunc = VecTargets.gen_hess(Vfunc, ReverseDiff.track.(x))
+    Hfunc = x -> _Hfunc(x)[3]
     # QUES What's the best output format of this function?
-    return x -> ReverseDiff.jacobian(x -> f(Hfunc(x)[3]), x) # default output shape [∂H∂x₁; ∂H∂x₂; ...]
+    return x -> ReverseDiff.jacobian(x -> f(Hfunc(x)), x) # default output shape [∂H∂x₁; ∂H∂x₂; ...]
 end
+
+# TODO Refactor this using https://juliadiff.org/ForwardDiff.jl/stable/user/api/#Preallocating/Configuring-Work-Buffers
+function gen_hess_fwd(func, x::AbstractVector)
+    function hess(x::AbstractVector)
+        return nothing, nothing, ForwardDiff.hessian(func, x)
+    end
+    return hess
+end
+
+function gen_∂G∂θ_fwd(Vfunc, x; f=identity)
+    _Hfunc = gen_hess_fwd(Vfunc, x)
+    Hfunc = x -> _Hfunc(x)[3]
+    # QUES What's the best output format of this function?
+    cfg = ForwardDiff.JacobianConfig(Hfunc, x)
+    d = length(x)
+    out = zeros(eltype(x), d^2, d)
+    return x -> ForwardDiff.jacobian!(out, Hfunc, x, cfg); out # default output shape [∂H∂x₁; ∂H∂x₂; ...]
+end
+# 1.764 ms 
+# fwd -> 5.338 μs 
+# cfg -> 3.651 μs
 
 function reshape_∂G∂θ(H)
     d = size(H, 2)
@@ -22,7 +44,7 @@ function prepare_sample_target(hps, θ₀, ℓπ)
         H = fstabilize(Hfunc(x)[3])
         any(.!(isfinite.(H))) ? diagm(ones(length(x))) : H
     end
-    _∂G∂θfunc = gen_∂G∂θ(Vfunc, θ₀; f=fstabilize) # size==(4, 2)
+    _∂G∂θfunc = gen_∂G∂θ_fwd(Vfunc, θ₀; f=fstabilize) # size==(4, 2)
     ∂G∂θfunc = x -> reshape_∂G∂θ(_∂G∂θfunc(x)) # size==(2, 2, 2)
 
     return Vfunc, Hfunc, Gfunc, ∂G∂θfunc
