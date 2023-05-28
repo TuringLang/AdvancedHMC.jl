a5486850921ea8bfd0deb417a7908c4c7c4b40e9
Kai Xu <xuk@amazon.com>
2023-05-28 12:24:46 +0000

    feat: implement both rel types

--- research/src/relativistic_hmc.jl
+++ research/src/relativistic_hmc.jl
@@ -22,7 +22,7 @@ function ∂H∂r(
     h::Hamiltonian{<:UnitEuclideanMetric,<:RelativisticKinetic},
     r::AbstractVecOrMat,
 )
-    mass = h.kinetic.m .* sqrt.(r .^ 2 ./ (h.kinetic.m .^ 2 * h.kinetic.c .^ 2) .+ 1)
+    mass = h.kinetic.m * sqrt(dot(r, r) / (h.kinetic.m^2 * h.kinetic.c^2) + 1)
     return r ./ mass
 end
 function ∂H∂r(
@@ -30,9 +30,38 @@ function ∂H∂r(
     r::AbstractVecOrMat,
 )
     r = h.metric.sqrtM⁻¹ .* r
+    mass = h.kinetic.m * sqrt(dot(r, r) / (h.kinetic.m^2 * h.kinetic.c^2) + 1)
+    red_term = r ./ mass # red part of (15)
+    return h.metric.sqrtM⁻¹ .* red_term # (15)
+end
+function ∂H∂r(h::Hamiltonian{<:DenseEuclideanMetric, <:RelativisticKinetic}, r::AbstractVecOrMat)
+    r = h.metric.cholM⁻¹ * r
+    mass = h.kinetic.m * sqrt(dot(r, r) / (h.kinetic.m^2 * h.kinetic.c^2) + 1)
+    red_term = r ./ mass
+    return h.metric.cholM⁻¹ * red_term
+end
+
+function ∂H∂r(
+    h::Hamiltonian{<:UnitEuclideanMetric,<:DimensionwiseRelativisticKinetic},
+    r::AbstractVecOrMat,
+)
+    mass = h.kinetic.m .* sqrt.(r .^ 2 ./ (h.kinetic.m .^ 2 * h.kinetic.c .^ 2) .+ 1)
+    return r ./ mass
+end
+function ∂H∂r(
+    h::Hamiltonian{<:DiagEuclideanMetric,<:DimensionwiseRelativisticKinetic},
+    r::AbstractVecOrMat,
+)
+    r = h.metric.sqrtM⁻¹ .* r
     mass = h.kinetic.m .* sqrt.(r .^ 2 ./ (h.kinetic.m .^ 2 * h.kinetic.c .^ 2) .+ 1)
     retval = r ./ mass # red part of (15)
     return h.metric.sqrtM⁻¹ .* retval # (15)
+end
+function ∂H∂r(h::Hamiltonian{<:DenseEuclideanMetric, <:DimensionwiseRelativisticKinetic}, r::AbstractVecOrMat)
+    r = h.metric.cholM⁻¹ * r
+    mass = h.kinetic.m .* sqrt.(r .^ 2 ./ (h.kinetic.m .^ 2 * h.kinetic.c .^ 2) .+ 1)
+    red_term = r ./ mass
+    return h.metric.cholM⁻¹ * red_term
 end
 
 
@@ -42,11 +71,10 @@ function neg_energy(
     θ::T,
 ) where {T<:AbstractVector}
     return -sum(
-        h.kinetic.m .* h.kinetic.c .^ 2 .*
-        sqrt.(r .^ 2 ./ (h.kinetic.m .^ 2 .* h.kinetic.c .^ 2) .+ 1),
+        h.kinetic.m * h.kinetic.c^2 *
+        sqrt(dot(r, r) / (h.kinetic.m^2 * h.kinetic.c^2) + 1),
     )
 end
-
 function neg_energy(
     h::Hamiltonian{<:DiagEuclideanMetric,<:RelativisticKinetic},
     r::T,
@@ -54,7 +82,51 @@ function neg_energy(
 ) where {T<:AbstractVector}
     r = h.metric.sqrtM⁻¹ .* r
     return -sum(
+        h.kinetic.m * h.kinetic.c^2 *
+        sqrt(dot(r, r) / (h.kinetic.m^2 * h.kinetic.c^2) + 1),
+    )
+end
+function neg_energy(
+    h::Hamiltonian{<:DenseEuclideanMetric,<:RelativisticKinetic},
+    r::T,
+    θ::T
+) where {T<:AbstractVector}
+    r = h.metric.cholM⁻¹ * r
+    return -sum(
+        h.kinetic.m * h.kinetic.c^2 * 
+        sqrt(dot(r, r) / (h.kinetic.m^2 * h.kinetic.c^2) + 1),
+    )
+end
+
+function neg_energy(
+    h::Hamiltonian{<:UnitEuclideanMetric,<:DimensionwiseRelativisticKinetic},
+    r::T,
+    θ::T,
+) where {T<:AbstractVector}
+    return -sum(
         h.kinetic.m .* h.kinetic.c .^ 2 .*
+        sqrt.(r .^ 2 ./ (h.kinetic.m .^ 2 .* h.kinetic.c .^ 2) .+ 1),
+    )
+end
+function neg_energy(
+    h::Hamiltonian{<:DiagEuclideanMetric,<:DimensionwiseRelativisticKinetic},
+    r::T,
+    θ::T,
+) where {T<:AbstractVector}
+    r = h.metric.sqrtM⁻¹ .* r
+    return -sum(
+        h.kinetic.m .* h.kinetic.c .^ 2 .*
+        sqrt.(r .^ 2 ./ (h.kinetic.m .^ 2 .* h.kinetic.c .^ 2) .+ 1),
+    )
+end
+function neg_energy(
+    h::Hamiltonian{<:DenseEuclideanMetric,<:DimensionwiseRelativisticKinetic},
+    r::T,
+    θ::T
+) where {T<:AbstractVector}
+    r = h.metric.cholM⁻¹ * r
+    return -sum(
+        h.kinetic.m .* h.kinetic.c .^ 2 .* 
         sqrt.(r .^ 2 ./ (h.kinetic.m .^ 2 .* h.kinetic.c .^ 2) .+ 1),
     )
 end
@@ -73,11 +145,10 @@ function _rand(
     densityfunc = x -> exp(neg_energy(h_temp, [x], [x]))
     sampler = RejectionSampler(densityfunc, (-Inf, Inf); max_segments = 5)
     sz = size(metric)
-    r = run_sampler!(rng, sampler, prod(sz))
+    r = run_sampler!(rng, sampler, prod(sz)) # FIXME!!! this sampler assumes dimensionwise!!!
     r = reshape(r, sz)
     return r
 end
-
 # TODO Support AbstractVector{<:AbstractRNG}
 function _rand(
     rng::AbstractRNG,
@@ -87,5 +158,53 @@ function _rand(
     r = _rand(rng, UnitEuclideanMetric(size(metric)), kinetic)
     # p' = A p where A = sqrtM
     r ./= metric.sqrtM⁻¹
+    return r
+end
+# TODO Support AbstractVector{<:AbstractRNG}
+function _rand(
+    rng::AbstractRNG,
+    metric::DenseEuclideanMetric{T},
+    kinetic::RelativisticKinetic{T},
+) where {T}
+    r = _rand(rng, UnitEuclideanMetric(size(metric)), kinetic)
+    # p' = A p where A = cholM
+    ldiv!(metric.cholM⁻¹, r)
+    return r
+end
+
+# TODO Support AbstractVector{<:AbstractRNG}
+function _rand(
+    rng::AbstractRNG,
+    metric::UnitEuclideanMetric{T},
+    kinetic::DimensionwiseRelativisticKinetic{T},
+) where {T}
+    h_temp = Hamiltonian(metric, kinetic, identity, identity)
+    densityfunc = x -> exp(neg_energy(h_temp, [x], [x]))
+    sampler = RejectionSampler(densityfunc, (-Inf, Inf); max_segments = 5)
+    sz = size(metric)
+    r = run_sampler!(rng, sampler, prod(sz))
+    r = reshape(r, sz)
+    return r
+end
+# TODO Support AbstractVector{<:AbstractRNG}
+function _rand(
+    rng::AbstractRNG,
+    metric::DiagEuclideanMetric{T},
+    kinetic::DimensionwiseRelativisticKinetic{T},
+) where {T}
+    r = _rand(rng, UnitEuclideanMetric(size(metric)), kinetic)
+    # p' = A p where A = sqrtM
+    r ./= metric.sqrtM⁻¹
+    return r
+end
+# TODO Support AbstractVector{<:AbstractRNG}
+function _rand(
+    rng::AbstractRNG,
+    metric::DenseEuclideanMetric{T},
+    kinetic::DimensionwiseRelativisticKinetic{T},
+) where {T}
+    r = _rand(rng, UnitEuclideanMetric(size(metric)), kinetic)
+    # p' = A p where A = cholM
+    ldiv!(metric.cholM⁻¹, r)
     return r
 end
