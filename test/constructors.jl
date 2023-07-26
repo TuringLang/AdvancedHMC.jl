0b55f5b77e2f1b4280b00febea09212f0c850ec2
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-07-26 13:07:20 +0100

    further improvements to tests

--- test/constructors.jl
+++ test/constructors.jl
@@ -6,10 +6,48 @@ include("common.jl")
     model = AbstractMCMC.LogDensityModel(ℓπ_gdemo)
 
     @testset "$T" for T in [Float32, Float64]
-        @testset "$(nameof(typeof(sampler)))" for sampler in [
-            HMC(T(0.1), 25),
-            HMCDA(T(0.1), 1),  # this should peform the correct promotion
-            NUTS(T(0.8))
+        @testset "$(nameof(typeof(sampler)))" for (sampler, expected) in [
+            (
+                HMC(T(0.1), 25),
+                (
+                    adaptor_type=NoAdaptation,
+                    metric_type=DiagEuclideanMetric{T},
+                    integrator_type=Leapfrog{T},
+                )
+            ),
+            # This should peform the correct promotion for the 2nd argument.
+            (
+                HMCDA(T(0.1), 1),
+                (
+                    adaptor_type=StanHMCAdaptor,
+                    metric_type=DiagEuclideanMetric{T},
+                    integrator_type=Leapfrog{T},
+                )
+            ),
+            (
+                NUTS(T(0.8)),
+                (
+                    adaptor_type=StanHMCAdaptor,
+                    metric_type=DiagEuclideanMetric{T},
+                    integrator_type=Leapfrog{T},
+                )
+            ),
+            (
+                NUTS(T(0.8); metric = :unit),
+                (
+                    adaptor_type=StanHMCAdaptor,
+                    metric_type=UnitEuclideanMetric{T},
+                    integrator_type=Leapfrog{T},
+                )
+            ),
+            (
+                NUTS(T(0.8); metric = :dense),
+                (
+                    adaptor_type=StanHMCAdaptor,
+                    metric_type=DenseEuclideanMetric{T},
+                    integrator_type=Leapfrog{T},
+                )
+            ),
         ]
             # Make sure the sampler element type is preserved.
             @test AdvancedHMC.sampler_eltype(sampler) == T
@@ -29,18 +67,9 @@ include("common.jl")
             @test eltype(transition.z.ℓκ.gradient) == T
 
             # Verify that the state is what we expect.
-            @test AdvancedHMC.getmetric(state) isa DiagEuclideanMetric{T, Vector{T}}
-            @test AdvancedHMC.getintegrator(state) isa Leapfrog{T}
-            # Sampler-specific.
-            if sampler isa HMC
-                @test AdvancedHMC.getadaptor(state) isa NoAdaptation
-            elseif sampler isa HMCDA
-                @test AdvancedHMC.getadaptor(state) isa StanHMCAdaptor
-            elseif sampler isa NUTS
-                @test AdvancedHMC.getadaptor(state) isa StanHMCAdaptor
-            else
-                error("$(sampler) not recognized")
-            end
+            @test AdvancedHMC.getmetric(state) isa expected.metric_type
+            @test AdvancedHMC.getintegrator(state) isa expected.integrator_type
+            @test AdvancedHMC.getadaptor(state) isa expected.adaptor_type
         end
     end
 end
