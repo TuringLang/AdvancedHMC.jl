6dc5d47f94d17aabca0bae37492d8131aa2605a4
Hong Ge <3279477+yebai@users.noreply.github.com>
2023-11-02 17:47:19 +0000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ using AdvancedHMC
 
 # cp(joinpath(@__DIR__, "../README.md"), joinpath(@__DIR__, "src/index.md"))
 
-makedocs(sitename = "AdvancedHMC", format = Documenter.HTML(), modules = [AdvancedHMC])
+makedocs(sitename = "AdvancedHMC", format = Documenter.HTML())
 
 deploydocs(
     repo = "github.com/TuringLang/AdvancedHMC.jl.git",
