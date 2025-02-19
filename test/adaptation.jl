3811a215871bd44668804a627cbbf8ca3de5fc05
David Widmann <devmotion@users.noreply.github.com>
2025-02-19 10:26:35 +0100

    Increase number of samples

--- test/adaptation.jl
+++ test/adaptation.jl
@@ -2,9 +2,9 @@ using ReTest, LinearAlgebra, Distributions, AdvancedHMC, Random, ForwardDiff
 using AdvancedHMC.Adaptation:
     WelfordVar, NaiveVar, WelfordCov, NaiveCov, get_estimation, get_estimation, reset!
 
-function runnuts(ℓπ, metric; n_samples = 3_000)
+function runnuts(ℓπ, metric; n_samples = 4_000)
     D = size(metric, 1)
-    n_adapts = 1_500
+    n_adapts = 2_000
     θ_init = rand(D)
     rng = MersenneTwister(0)
 
