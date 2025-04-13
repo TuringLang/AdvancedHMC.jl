b72310c6ef025ccccfa75fe33bb2054adf58d86e
Qingyu Qu <2283984853@qq.com>
2025-04-13 19:41:39 +0800

    Apply reviews

--- ext/AdvancedHMCComponentArraysExt.jl
+++ ext/AdvancedHMCComponentArraysExt.jl
@@ -12,30 +12,28 @@ using AdvancedHMC:
 using ComponentArrays: ComponentVecOrMat, ComponentVector, ComponentMatrix, getaxes
 
 function AdvancedHMC.∂H∂r(
-    h::Hamiltonian{<:UnitEuclideanMetric,<:GaussianKinetic}, r::ComponentVecOrMat
-)
-    return copy(r)
-end
-function AdvancedHMC.∂H∂r(
     h::Hamiltonian{<:DiagEuclideanMetric,<:GaussianKinetic}, r::ComponentVecOrMat
 )
     (; M⁻¹) = h.metric
-    @assert getaxes(M⁻¹) == getaxes(r) "Axes of mass matrix and momentum must match"
+    (getaxes(M⁻¹) !== getaxes(r)) &&
+        throw(ArgumentError("Axes of mass matrix and momentum must match"))
     return h.metric.M⁻¹ .* r
 end
 function AdvancedHMC.∂H∂r(
     h::Hamiltonian{<:DenseEuclideanMetric,<:GaussianKinetic}, r::ComponentVector
 )
     (; M⁻¹) = h.metric
-    @assert (first(getaxes(M⁻¹)) == first(getaxes(r))) &&
-        (last(getaxes(M⁻¹)) == first(getaxes(r))) "Axes of mass matrix and momentum must match"
+    (first(getaxes(M⁻¹)) !== first(getaxes(r))) ||
+        (last(getaxes(M⁻¹)) !== first(getaxes(r))) &&
+            throw(ArgumentError("Axes of mass matrix and momentum must match"))
     return h.metric.M⁻¹ * r
 end
 function AdvancedHMC.∂H∂r(
     h::Hamiltonian{<:DenseEuclideanMetric,<:GaussianKinetic}, r::ComponentMatrix
 )
     (; M⁻¹) = h.metric
-    @assert getaxes(M⁻¹) == getaxes(r) "Axes of mass matrix and momentum must match"
+    getaxes(M⁻¹) !== getaxes(r) &&
+        throw(ArgumentError("Axes of mass matrix and momentum must match"))
     return h.metric.M⁻¹ * r
 end
 
