0fc251ab83d5d7af2c4271b96b34c78eb1cd3746
Hong Ge <3279477+yebai@users.noreply.github.com>
2025-04-30 20:32:25 +0100

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/CUDA/cuda.jl
+++ test/CUDA/cuda.jl
@@ -15,18 +15,15 @@ include(joinpath(@__DIR__, "..", "common.jl"))
         n_chains = 1000
         n_samples = 1000
         dim = 5
-    
         T = Float32
         m, s, θ₀ = zeros(T, dim), ones(T, dim), rand(T, dim, n_chains)
         m, s, θ₀ = CuArray(m), CuArray(s), CuArray(θ₀)
-    
         target = Gaussian(m, s)
         metric = UnitEuclideanMetric(T, size(θ₀))
         ℓπ, ∇ℓπ = get_ℓπ(target), get_∇ℓπ(target)
         hamiltonian = Hamiltonian(metric, ℓπ, ∇ℓπ)
         integrator = Leapfrog(one(T) / 5)
         proposal = HMCKernel(Trajectory{EndPointTS}(integrator, FixedNSteps(5)))
-    
         samples, stats = sample(hamiltonian, proposal, θ₀, n_samples)
     else
         println("GPU tests are skipped because no CUDA devices are found.")
@@ -52,10 +49,8 @@ end
                     DualValue(CuArray(zeros(T, 2)), CuArray(zeros(T, 1, 2))),
                 )
             end
-    
             z1 = init_z1()
             z2 = init_z2()
-    
             @test z1.ℓπ.value == z2.ℓπ.value
             @test z1.ℓκ.value == z2.ℓκ.value
         end
