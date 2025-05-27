14047cfb3d55d3c455c010db4c8e8dc37422db16
Qingyu Qu <2283984853@qq.com>
2025-05-27 23:10:07 +0800

    Restrict AbstractTrajectorySampler

--- src/trajectory.jl
+++ src/trajectory.jl
@@ -220,7 +220,11 @@ function Trajectory{TS}(integrator::I, termination_criterion::TC) where {TS,I,TC
     return Trajectory{TS,I,TC}(integrator, termination_criterion)
 end
 
-ConstructionBase.constructorof(::Type{<:Trajectory}) = Trajectory
+function ConstructionBase.constructorof(
+    ::Type{<:Trajectory{TS}}
+) where {TS<:AbstractTrajectorySampler}
+    return Trajectory{TS}
+end
 
 function Base.show(io::IO, mime::MIME"text/plain", τ::Trajectory{TS}) where {TS}
     return print(
