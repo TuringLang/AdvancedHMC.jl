85adcbd70c15d48cd220ec98e84ccda831126b34
David Müller-Widmann <devmotion@users.noreply.github.com>
2025-09-11 09:13:06 +0000

    Fix `show` definitions (#466)
    * Fix `show` definitions
    
    * Update src/integrator.jl
    
    Co-authored-by: Xianda Sun <5433119+sunxd3@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Xianda Sun <5433119+sunxd3@users.noreply.github.com>

--- src/adaptation/stan_adaptor.jl
+++ src/adaptation/stan_adaptor.jl
@@ -49,7 +49,7 @@ function initialize!(
     return nothing
 end
 
-function Base.show(io::IO, mime::MIME"text/plain", state::StanHMCAdaptorState)
+function Base.show(io::IO, state::StanHMCAdaptorState)
     print(io, "window(", state.window_start, ", ", state.window_end, "), window_splits(")
     join(io, state.window_splits, ", ")
     return print(io, ")")
@@ -65,6 +65,23 @@ struct StanHMCAdaptor{M<:MassMatrixAdaptor,Tssa<:StepSizeAdaptor} <: AbstractAda
     term_buffer::Int
     window_size::Int
     state::StanHMCAdaptorState
+end
+
+function Base.show(io::IO, a::StanHMCAdaptor)
+    return print(
+        io,
+        "StanHMCAdaptor(",
+        a.pc,
+        ", ",
+        a.ssa,
+        "; init_buffer=",
+        a.init_buffer,
+        ", term_buffer=",
+        a.term_buffer,
+        ", window_size=",
+        a.window_size,
+        ")",
+    )
 end
 function Base.show(io::IO, mime::MIME"text/plain", a::StanHMCAdaptor)
     return print(
